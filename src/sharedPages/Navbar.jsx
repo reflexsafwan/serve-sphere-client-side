f46dc6c496fb1152395a15127117a6720b3f6ae3
reflexsafwan <reflex.safwan@gmail.com>
2025-09-28 13:44:58 +0600

    change nav width container to full

--- src/sharedPages/Navbar.jsx
+++ src/sharedPages/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm container mx-auto mt-4">
+      <div className="navbar bg-base-100 shadow-sm  mx-auto mt-4">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
