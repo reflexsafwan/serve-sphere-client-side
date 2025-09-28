cb438aadfc9f1a4800214bdcac4bcaadc3b43372
reflexsafwan <reflex.safwan@gmail.com>
2025-09-28 13:53:13 +0600

    changed volunteer card layout 3 column to four column in lg and for md chnge to 3 column

--- src/components/VolunteerPostsForHome.jsx
+++ src/components/VolunteerPostsForHome.jsx
@@ -45,7 +45,7 @@ const VolunteerPostsForHome = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-6">
           {VolunteerNeedsData?.map((post) => (
             <SingleVolunteerNeed
               post={post}
