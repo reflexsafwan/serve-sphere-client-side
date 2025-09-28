38a753034d398cf75be7f85a5a9c5d8c7e4c4114
reflexsafwan <reflex.safwan@gmail.com>
2025-09-28 13:26:06 +0600

    navbar sticky position added with top-0 and added z index also and fix navbar font size to base size

--- src/layouts/RootLayouts.jsx
+++ src/layouts/RootLayouts.jsx
@@ -6,7 +6,7 @@ import { Outlet } from "react-router";
 const RootLayouts = () => {
   return (
     <div>
-      <div>
+      <div className="sticky top-0 z-100">
         <Navbar></Navbar>
       </div>
       <Outlet></Outlet>
--- src/sharedPages/Navbar.jsx
+++ src/sharedPages/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
       <NavLink
         className={({ isActive }) =>
           isActive
-            ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg  "
-            : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg "
+            ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg text-base "
+            : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg text-base "
         }
         to={"/"}
       >
@@ -21,8 +21,8 @@ const Navbar = () => {
       <NavLink
         className={({ isActive }) =>
           isActive
-            ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg  "
-            : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg "
+            ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg  text-base "
+            : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg text-base"
         }
         to={"/all-volunteer-need-posts"}
       >
@@ -33,8 +33,8 @@ const Navbar = () => {
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg  "
-              : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg "
+              ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg text-base  "
+              : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg text-base "
           }
           to={"/my-volunteer-need-post"}
         >
@@ -45,8 +45,8 @@ const Navbar = () => {
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg  "
-              : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg "
+              ? "bg-[#ED4C67] text-white px-3 py-2 rounded-lg text-base "
+              : "hover:bg-[#ED4C67] hover:text-white px-3 py-2 rounded-lg text-base"
           }
           to={"/my-volunteer-request"}
         >
